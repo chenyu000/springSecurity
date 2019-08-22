cd9df182536d29d2675b47008fc5e982ba140a4a
cy <479523594@qq.com>
2019-08-22 13:12:56 +0800

    3

--- src/main/java/com/baizhi/controller/UserController.java
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @RequestMapping("index")
+
     public String index(){
         return "index";
     }
